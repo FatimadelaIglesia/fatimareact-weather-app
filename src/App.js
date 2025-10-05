651a384f1735749a4647868453154745a5d97df5
FatimadelaIglesia <fatimaiglesiav@gmail.com>
2025-10-05 21:04:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by Fatima de la Iglesia and is{" "}
+        <a
+          href="https://github.com/FatimadelaIglesia/fatimareact-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
