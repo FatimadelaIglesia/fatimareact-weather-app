e793d544bf6d4a2605b0a4dd3a60124ae4ac8c03
FatimadelaIglesia <fatimaiglesiav@gmail.com>
2025-10-19 17:48:05 +0100

    Update WeatherTemperature.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
   const [unit, setUnit] = useState("celsius");
+
   function showFahrenheit(event) {
     event.preventDefault();
     setUnit("fahrenheit");
   }
+
   function showCelsius(event) {
     event.preventDefault();
     setUnit("celsius");
   }
+
   function fahrenheit() {
     return (props.celsius * 9) / 5 + 32;
   }
@@ -17,7 +20,9 @@ export default function WeatherTemperature(props) {
   if (unit === "celsius") {
     return (
       <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="temperature">
+          {props.celsius ? Math.round(props.celsius) : "–"}
+        </span>
         <span className="unit">
           ℃ ∣{" "}
           <a href="/" onClick={showFahrenheit}>
@@ -29,10 +34,12 @@ export default function WeatherTemperature(props) {
   } else {
     return (
       <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahrenheit)}</span>
+        <span className="temperature">
+          {props.celsius ? Math.round(fahrenheit()) : "–"}
+        </span>
         <span className="unit">
           <a href="/" onClick={showCelsius}>
-            °C /
+            °C
           </a>{" "}
           ∣ °F
         </span>
