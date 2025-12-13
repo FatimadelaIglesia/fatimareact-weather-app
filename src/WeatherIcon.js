eb93fffdd72e3335ddd0b075582cdd0ad1d2c476
FatimadelaIglesia <fatimaiglesiav@gmail.com>
2025-12-13 17:40:42 +0000

    weather updated

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#c61b43ff"
-      size={52}
+      size={props.size}
       animate={true}
     />
   );
