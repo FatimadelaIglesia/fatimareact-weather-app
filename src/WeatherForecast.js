20e4a124ff7dc8ff38fec08f5360bb257ac60926
FatimadelaIglesia <fatimaiglesiav@gmail.com>
2025-12-13 18:46:55 +0000

    axios call

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,15 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
-export default function WeatherForecast() {
+import axios from "axios";
+export default function WeatherForecast(props) {
+  function handleResponse(response) {}
+
+  let apiKey = "c6f8ef4575250284954db9f4dfa7a996";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
