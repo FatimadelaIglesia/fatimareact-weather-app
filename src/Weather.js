c9f7a3c4045e9db8e276fd3c8b85041de2efc8e2
FatimadelaIglesia <fatimaiglesiav@gmail.com>
2025-12-14 16:01:08 +0000

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,37 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Weather.css";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
-  const [WeatherData, setWeatherData] = useState({ ready: false });
+  const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
 
+  useEffect(() => {
+    search();
+    // eslint-disable-next-line
+  }, []);
+
   function handleResponse(response) {
-    console.log(response.data);
     setWeatherData({
       ready: true,
-      coordinates: response.data.coordinates,
-      temperature: response.data.temperature?.current || 0,
-      humidity: response.data.temperature?.humidity || 0,
-      wind: response.data.wind.speed || 0,
-      city: response.data.city || city,
-      date: new Date(response.data.time * 1000),
-      description: response.data.condition?.description || "",
-      iconURL:
-        response.data.condition?.icon_url ||
-        "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-      iconCode: response.data.condition?.icon || "",
+      coordinates: {
+        lat: response.data.coord.lat,
+        lon: response.data.coord.lon,
+      },
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      iconCode: response.data.weather[0].icon,
     });
   }
+
   function search() {
     const apiKey = "3944928100fff63d399c10d8e59aaf14";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-    let apiURL = `https://api.openweathermap.org/data/2.5/current?query=${city}&key=${apiKey}&units=metric`;
-
-    axios.get(apiURL).then(handleResponse);
+    axios
+      .get(apiUrl)
+      .then(handleResponse)
+      .catch((error) => {
+        console.error("Current weather error:", error);
+      });
   }
+
   function handleSubmit(event) {
     event.preventDefault();
     search();
@@ -41,39 +51,35 @@ export default function Weather(props) {
     setCity(event.target.value);
   }
 
-  if (WeatherData.ready) {
-    return (
-      <div className="Weather">
-        <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city.."
-                className="form-control"
-                autoFocus="on"
-                onChange={handleCityChange}
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
-            </div>
+  return (
+    <div className="Weather">
+      <form onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus
+              onChange={handleCityChange}
+            />
           </div>
-        </form>
-        <WeatherInfo data={WeatherData} />
-        <WeatherForecast
-          coordinates={{
-            lon: WeatherData.coordinates.longitude,
-            lat: WeatherData.coordinates.latitude,
-          }}
-        />
-      </div>
-    );
-  } else {
-    search();
-  }
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+
+      {weatherData.ready && (
+        <>
+          <WeatherInfo data={weatherData} />
+          <WeatherForecast coordinates={weatherData.coordinates} />
+        </>
+      )}
+    </div>
+  );
 }
